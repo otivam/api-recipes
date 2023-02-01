5ad905497c23a83ea95df9375555c3a77e833d16
Gincho <otivam@gmail.com>
2023-02-01 10:35:58 +0200

    fixing bugs

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request, redirect
 import requests
 
 app = Flask(__name__, static_url_path="/static")
-
 url = "https://tasty.p.rapidapi.com/recipes/list"
-
 headers = {
-	"X-RapidAPI-Key": "",
+	"X-RapidAPI-Key": "2ad60a66c2msh449e56015dac732p1a7c37jsn1403adeafdd5",
 	"X-RapidAPI-Host": "tasty.p.rapidapi.com"
 }
+
+
 
 @app.route("/")
 def home_page():
@@ -20,31 +20,34 @@ def search():
 
     querystring = {"from":"0","size":"100","q":data_ingridients}
     response = requests.request("GET", url, headers=headers, params=querystring)
-    all_recipes = response.json()["results"]
-    all_recipes_sorted = []
     cc = 10
     cc_page = 1
+    pagination = ""
+    all_recipes_sorted = []
 
-    for recipe in all_recipes:
-        if recipe["description"] == "" or recipe["description"] == None:
-            all_recipes_sorted.append(recipe)
-        else:
-            all_recipes_sorted.insert(0,recipe)
+    if response.json()["count"] > 0:
+        all_recipes = response.json()["results"]
+        pagination = "True"
 
-    for i, recipe in enumerate(all_recipes_sorted):
-        recipe["page"] = cc_page
-        cc -= 1
+        for recipe in all_recipes:
+            if recipe["description"] == "" or recipe["description"] == None:
+                all_recipes_sorted.append(recipe)
+            else:
+                all_recipes_sorted.insert(0,recipe)
 
-        if i != len(all_recipes_sorted)-1:
-            if cc == 0:
-                cc = 10
-                cc_page += 1
+        for i, recipe in enumerate(all_recipes_sorted):
+            recipe["page"] = cc_page
+            cc -= 1
 
+            if i != len(all_recipes_sorted)-1:
+                if cc == 0:
+                    cc = 10
+                    cc_page += 1
 
     return render_template(
         "base.html",
         recipes = all_recipes_sorted,
-        pagination = "True",
+        pagination = pagination,
         cc_page = cc_page
     )
 
