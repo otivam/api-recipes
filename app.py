79f7a3aeaf85e3a24b23af849771fa0742b9e9d2
Gincho Georgiev <otivam@gmail.com>
2024-01-27 10:08:23 +0200

    adding modals

--- app.py
+++ app.py
@@ -1,12 +1,124 @@
-from flask import Flask, render_template, request, redirect, url_for, session, make_response, Response
-import requests, urllib.request
+from flask import Flask, render_template, request, redirect, url_for, session, make_response, Response, jsonify
+import requests, urllib.request, sqlite3, json
+
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+
 
 app = Flask(__name__, static_url_path="/static")
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+login_manager = LoginManager()
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin):   
+    def __init__(self, name, id, active):
+        self.name = name
+        self.id = str(id)
+        self.active = active                
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):   
+        return self.active           
+
+    def is_anonymous(self):
+        return False          
+
+    def get_id(self):         
+        return str(self.id)
 
 
 
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    connection = sqlite3.connect("users.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `users` WHERE `id` = ?", (user_id))
+    results = cursor.fetchall()
+    connection.close()
+
+    UserObject = User(results[0][1], user_id, True)
+    if UserObject.get_id() == user_id:
+        return UserObject
+    else:
+        return None
+
+
+
+@app.route('/login', methods=['POST'])
+def login():
+  if request.method == "POST":
+    data = request.get_json()
+
+    connection = sqlite3.connect("users.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `users` WHERE `username` = ? AND `password` = ?", (data["username"], data["pass"]))
+    results = cursor.fetchall()
+    connection.close()
+
+    if results != []:
+        result_id = str(results[0][0])
+        UserObject = User(results[0][1], result_id, active=True)
+        login_user(UserObject)
+
+        if not results:
+            return jsonify(status="0")
+        else:
+            return jsonify(status="1", username=UserObject.name, id=UserObject.id)
+    else:
+        return jsonify(status="0")
+
+    
+    
+@app.route("/logout", methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify(status="1")
+
+
+
+
+
+
+@app.route('/register-user', methods=['POST'])
+def register_user():
+  if request.method == "POST":
+    data = request.get_json()
+
+    connection = sqlite3.connect("users.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO users (username, email, password) VALUES (?,?,?)", (data["username"], data["email"], data["pass"]))
+    connection.commit()
+    connection.close()
+    
+    return jsonify(status="1")
+  
+
+@app.route('/check-username', methods=['POST'])
+def check_username():
+  if request.method == "POST":
+    data = (request.get_json())
+
+    connection = sqlite3.connect("users.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT EXISTS (SELECT 1 FROM users WHERE username = ?)", (data["username"],))
+    
+    if cursor.fetchone()[0]:
+        connection.close()
+        return jsonify(status="1")
+    else:
+        connection.close()
+        return jsonify(status="0")
+    
+    
+    
 
 
 
@@ -82,26 +194,7 @@ def recipe_details(recipe_id):
             
     return render_template('recipe.html', recipe=json_response)
 
-"""
-@app.route('/get_recipe_video', methods=['POST'])
-def get_recipe_video():
-  if request.method == "POST":
-    recipe_id = request.get_data(cache=False).decode('UTF-8')
-    
-    url = "https://tasty.p.rapidapi.com/recipes/get-more-info"
-    headers = {
-	"X-RapidAPI-Key": "2ad60a66c2msh449e56015dac732p1a7c37jsn1403adeafdd5",
-	"X-RapidAPI-Host": "tasty.p.rapidapi.com"
-    }
-    querystring = {"id":str(recipe_id)}
-    response = requests.get(url, headers=headers, params=querystring)
-    video_url = response.json()["original_video_url"]
 
-    resp = Response(video_url)
-    resp.headers["Content-Type"] = "video/mp4"
-    
-    return resp
-"""
 
 
 @app.route("/search/<slug>")
--- static/master.js
+++ static/master.js
@@ -1,24 +1,20 @@
 "use strict";
 
 $(window).on("load", function(){
-    /*
-    var recipe_id = window.location.href
-    console.log(recipe_id.split('/')[4])
+    if($("input[name='is-logged-in']").val() == "1"){
+        $(".breadcrumb .register, .breadcrumb .login").hide();
+        $(".breadcrumb .logout").show();
+        $(".breadcrumb .logout").addClass("breadcrumb-item");
+    }else if($("input[name='is-logged-in']").val() == "0"){
+        $(".breadcrumb .register, .breadcrumb .login").show();
+        $(".breadcrumb .logout").hide();
+        $(".breadcrumb .logout").removeClass("breadcrumb-item");
+    }
 
-    
-    
-    $.ajax({
-        type: "POST",
-        url: "/get_recipe_video",
-        data: recipe_id.split('/')[4],
-        contentType: "application/json",
-        dataType: 'text',
-        success: function(result){
-            console.log(result);
-            $("#recipe_video").attr("src", result)
-        } 
-    });
-    */
+
+
+
+
 
     //VIDEO MODAL FUNCTIONALITY
     $("tbody img").each(function(){
@@ -55,5 +51,284 @@ $(window).on("load", function(){
             $("#details_modal_" + grandparent).modal("show");
         })
     })
+
+    function clearRegisterInputs(){
+        $("p[class^='text-danger validation-register']").hide(); //hide the validations
+        $("input[name^='register-']").val(""); //empty the inputs
+    }
+
+
+    $(".register-login .register").on("click", function(){
+        clearRegisterInputs();
+        $("#register_modal").modal("show");
+    })
+
+    $(".register-login .login").on("click", function(){
+        $("#login_modal").modal("show");
+    })
+
+    //VALIDATIONS 
+    $(".text-danger, .register-submit").hide();
+
+    function checkAllValidations_register(){
+        var register_validation = [repeat_pass(), pass_length(), pass_strength(), validate_email(), ],
+            email_validation = $("input[name='register-email']").val() != "",
+            username_validation = $("input[name='register-username']").val() != "" && $(".validation-register-username").is(":hidden");
+
+        if(($.inArray(false, register_validation) === -1) && email_validation && username_validation){
+            $(".register-submit").show();
+            return true;
+        }else{
+            $(".register-submit").hide();
+            return false;
+        }
+    }
+
+    function pass_length(){
+        var pass = $("input[name='register-pass']").val();
+
+        if(pass != ""){
+            if(pass.length < 8){
+                $(".validation-register-pass-length").show();
+                return false;
+            }else{
+                $(".validation-register-pass-length").hide();
+                return true;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    function containsUppercase(str) {
+        return /[A-Z]/.test(str);
+    }
+
+    function containsSpecialCharacter(str) {
+        return /[^A-Za-z 0-9]/g.test(str);
+    }
+
+    function pass_strength(){
+        var pass = $("input[name='register-pass']").val();
+
+        if(pass != ""){
+            if(!containsUppercase(pass) || !containsSpecialCharacter(pass)){
+                $(".validation-register-pass-strength").show();
+                return false;
+            }else{
+                $(".validation-register-pass-strength").hide();
+                return true;
+            }
+        }else{
+            return false;
+        }
+
+    }
+
+    $("input[name='register-pass']").on("change", function(){
+        pass_length();
+        pass_strength();
+        repeat_pass();
+        checkAllValidations_register();
+    })
+
+
+
+    function repeat_pass(){
+        var pass = $("input[name='register-pass']").val(),
+            r_pass = $("input[name='register-repeat-pass']").val();
+
+        if(pass != "" && r_pass != ""){
+            if(pass != r_pass){
+                $(".validation-register-repeat-pass").show();
+                return false
+            }else{
+                $(".validation-register-repeat-pass").hide();
+                return true
+            }
+        }else{
+            return false
+        }
+    }
+
+
+    $("input[name='register-repeat-pass']").on("change", function(){
+        repeat_pass();
+        checkAllValidations_register();
+    })
+        
+
+    $("input[name='register-username']").on("change", function(){
+        var username = $("input[name='register-username']").val();
+
+        $.ajax({
+            type: "POST",
+            url: "/check-username",
+            data: JSON.stringify({"username":username}),
+            contentType: "application/json",
+            dataType: 'json',
+            success: function(result){
+                if(result["status"] == '1'){
+                    $(".validation-register-username").show();
+                }else{
+                    $(".validation-register-username").hide();
+                }
+                checkAllValidations_register();
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    })
+
+
+    $("input[name='register-email']").on("change", function(){
+        checkAllValidations_register();
+    })
+
+
+
+    var validateEmail = (email) => {
+        return email.match(
+          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    };
+
+    function validate_email(){
+        var email = $("input[name='register-email']").val();
+
+        if(email != ""){
+            if(validateEmail(email)){
+                $(".validation-register-email").hide();
+                return true;
+            }else{
+                $(".validation-register-email").show();
+                return false;
+            }
+        }else{
+            return false;
+        }
+
+    }
+
+    $("input[name='register-email']").on("change", function(){
+        validate_email();
+    })
+
+
+    // SHOW SUBMIT BUTTON IF USERNAME AND PASSWORDS ARE FILLED IN
+    function checkIfEmpty(){
+        var username = $("input[name='login-username']").val(),
+            password = $("input[name='login-pass']").val();
+
+        if(username != '' && password != ''){
+            $(".login-submit").removeAttr('hidden');
+        }else{
+            $(".login-submit").attr('hidden', true);
+        }
+    }
+
+    $("input[name='login-username']").on("change", function(){
+        checkIfEmpty();
+    })
+
+    $("input[name='login-pass']").on("change", function(){
+        checkIfEmpty();
+    })
+
+    //REGISTER/LOGIN - IF CLOSE IS CLICKED -> REDIRECT TO HOME PAGE
+    $(".successful-registration-close, .welcome-close").on("click", function(){
+        document.location.href="/";
+    })
+
+
+    $(".breadcrumb-item.logout").on("click", function(){
+        $("#logout_modal").modal("show");
+    })
+
+
+    $(".register-submit").on("click", function(){
+        var email = $("input[name='register-email']").val(),
+            username = $("input[name='register-username']").val(),
+            pass = $("input[name='register-pass']").val();
+
+        $(".register-submit").hide();
+        $('<div class="spinner-border spinner-register" style="position:absolute; bottom:-18%;" role="status"><span class="sr-only"></span></div>').insertAfter(".register-submit");
+        
+        $.ajax({
+            type: "POST",
+            url: "/register-user",
+            data: JSON.stringify({"email":email, "username":username, "pass":pass}),
+            contentType: "application/json",
+            dataType: 'json',
+            success: function(result){
+                console.log(result);
+                if(result["status"] == '1'){
+                    clearRegisterInputs();
+                    $("#register_modal").find(".register-close").trigger("click");
+                    $("#successful_register_modal").modal("show");
+                    $(".spinner-register").hide();
+                }
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+        
+    })
+
+    $(".login-submit").on("click", function(){
+        var username = $("input[name='login-username']").val(),
+            pass = $("input[name='login-pass']").val();
+
+        $.ajax({
+            type: "POST",
+            url: "/login",
+            data: JSON.stringify({"username":username, "pass":pass}),
+            contentType: "application/json",
+            dataType: 'json',
+            success: function(result){
+                console.log(result);
+                if(result["status"] == "0"){
+                    $(".validation-login").show();
+                }else{
+                    $(".validation-login").hide();
+                    $(".breadcrumb .register, .breadcrumb .login").hide();
+                    $(".breadcrumb .logout").show();
+                    $(".breadcrumb .logout").addClass("breadcrumb-item");
+
+                    $("#login_modal").find(".login-close").trigger("click");
+                    $("#successful_login_modal").modal("show");
+                }
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    })
+
+    $(".breadcrumb .logout").on("click", function(){
+        $(this).hide();
+        $(".breadcrumb .register,.breadcrumb .login").show();
+        $(".breadcrumb .logout").removeClass("breadcrumb-item");
+
+        $.ajax({
+            type: "POST",
+            url: "/logout",
+            contentType: "application/json",
+            dataType: 'json',
+            success: function(result){
+                console.log(result);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    })
+
+
+
+
+
 
 })
